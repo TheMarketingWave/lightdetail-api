67fbc7767cd73e2d000a593e0abf03163c7b1d87
radusi.stefan <radusi.stefan@gmail.com>
2026-02-27 15:16:25 +0200

    add health

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -3,29 +3,54 @@ import { createRouter } from "../lib/create-app";
 import jsonContent from "../middlewares/helpers/ json-content";
 import { OK } from "../middlewares/helpers/http-status-codes";
 
-const router = createRouter().openapi(
-  createRoute({
-    tags: ["home"],
-    method: "get",
-    path: "/",
-    responses: {
-      [OK]: jsonContent(
-        z
-          .object({
-            msg: z.string(),
-          })
-          .openapi({
-            example: {
-              msg: "Example string",
-            },
-          }),
-        "LightDetail API Index",
-      ),
+const router = createRouter()
+  .openapi(
+    createRoute({
+      tags: ["home"],
+      method: "get",
+      path: "/",
+      responses: {
+        [OK]: jsonContent(
+          z
+            .object({
+              msg: z.string(),
+            })
+            .openapi({
+              example: {
+                msg: "Example string",
+              },
+            }),
+          "LightDetail API Index",
+        ),
+      },
+    }),
+    (c) => {
+      return c.json({ msg: "Works" }, OK);
     },
-  }),
-  (c) => {
-    return c.json({ msg: "Works" }, OK);
-  },
-);
+  )
+  .openapi(
+    createRoute({
+      tags: ["health"],
+      method: "get",
+      path: "/health",
+      responses: {
+        [OK]: jsonContent(
+          z
+            .object({
+              status: z.string(),
+            })
+            .openapi({
+              example: {
+                status: "ok",
+              },
+            }),
+          "Health check for Coolify",
+        ),
+      },
+    }),
+    (c) => {
+      return c.json({ status: "ok" }, OK);
+    },
+  );
 
 export default router;
